ce8dd0c610dd205f41a170eff39f69729bfe7c49
Andrés Felipe Zúñiga <50376585+Zuniga63@users.noreply.github.com>
2023-02-27 12:08:30 -0500

    Initialize boxes

---
+++ src/utils/index.ts
@@ -0,0 +1,29 @@
+/**
+ * Util for create a simple slug
+ * @param {string} text Texto a normalizar
+ * @returns String
+ */
+export const createSlug = (text: string): string => {
+  return text
+    ? text
+        .trim()
+        .toLocaleLowerCase()
+        .replace(/\s/gi, '-')
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+    : '';
+};
+
+export const emailRegex = /^[^@]+@[^@]+.[^@]+$/;
+
+export const strongPass =
+  /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/;
+
+export interface IImage {
+  publicId: string;
+  width: number;
+  height: number;
+  format: string;
+  type: string;
+  url: string;
+}
